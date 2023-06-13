f236d54ee69520a0a2aa5b81959663decb862a32
Nicolas Lucio <nilucio65@gmail.com>
2023-06-13 08:45:52 -0300

    Cria componentes, services e interfaces

---
+++ src/app/anuncio.ts
@@ -0,0 +1,7 @@
+export interface Anuncio{
+  id: number;
+  url: string;
+  tipo: string;
+  anunciante: string;
+  preco: number;
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AnuncioComponent } from './anuncio/anuncio.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AnuncioComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
