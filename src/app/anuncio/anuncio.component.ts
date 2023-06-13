9e465b8096abbcfc760c12aa81e4d4048f9349bf
Nicolas Lucio <nilucio65@gmail.com>
2023-06-13 09:51:11 -0300

    Implementa o cadastro de Anúncios

--- src/app/anuncio/anuncio.component.ts
+++ src/app/anuncio/anuncio.component.ts
@@ -1,4 +1,7 @@
+import { AnuncioService } from './../anuncio.service';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Anuncio } from '../anuncio';
 
 @Component({
   selector: 'app-anuncio',
@@ -6,5 +9,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./anuncio.component.css']
 })
 export class AnuncioComponent {
+
+  anuncio: Anuncio[] = [];
+  formGroupAnuncio!: FormGroup;
+  isEditing: boolean = false;
+
+  constructor (private AnuncioService: AnuncioService, private formBuilder: FormBuilder){
+    this.formGroupAnuncio = formBuilder.group({
+      id: [''],
+      url: [''],
+      tipo: [''],
+      anunciante: [''],
+      preco: ['']
+    })
+  }
+
+  Save(){
+
+    this.AnuncioService.save(this.formGroupAnuncio.value).subscribe(
+      {
+        next: data =>{
+          this.anuncio.push(data);
+        }
+      }
+    )
+    }
+
+
+
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AnuncioComponent } from './anuncio/anuncio.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    AnuncioComponent
+    AnuncioComponent,
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
